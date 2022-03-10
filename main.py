766e288f3b85679c9fdfeabfb0893c73f3f5f673
Harry <vin-ig@yandex.ru>
2022-03-10 20:46:51 +0300

    Comments is added

--- main.py
+++ main.py
@@ -6,19 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def page_index():
+    """Главная страница"""
     result = '<pre>'
-    for candidate in candidates:
+    for candidate in candidates:  # Выводим общий список кандидатов
         result += f"Имя кандидата: {candidate.get('name')}\nПозиция кандидата: {candidate.get('position')}\n" \
                   f"Навыки: {candidate.get('skills')}\n\n"
     result += '</pre>'
+
     return result
 
 
 @app.route('/candidate/<name>')
 def page_profile(name):
+    """Страница с данными кандидата"""
     result = '<pre>\n'
     for candidate in candidates:
-        if candidate.get('name').lower() == name.lower():
+        if candidate.get('name').lower() == name.lower():  # Ищем совпадение по имени
             result += f"<img src='{candidate.get('picture')}'>\n\n" \
                       f"Имя кандидата: {candidate.get('name')}\nПозиция кандидата: {candidate.get('position')}\n" \
                       f"Навыки: {candidate.get('skills')}\n\n"
@@ -26,22 +29,24 @@ def page_profile(name):
     else:
         result += 'Кандидат не найден'
     result += '</pre>'
+
     return result
 
 
 @app.route('/skill/<skill>')
 def page_skill(skill):
+    """Поиск кандидата по навыкам"""
     flag = False
     result = '<pre>\n'
     for candidate in candidates:
-        skills = [elem.lower() for elem in candidate.get('skills').split(', ')]
-        if skill.lower() in skills:
+        if skill.lower() in candidate.get('skills').lower():  # Ищем совпадение по навыкам
             result += f"Имя кандидата: {candidate.get('name')}\nПозиция кандидата: {candidate.get('position')}\n" \
                       f"Навыки: {candidate.get('skills')}\n\n"
             flag = True
     if not flag:
         result += 'Кандидата с такими навыками не найдено'
     result += '</pre>'
+
     return result
 
 
